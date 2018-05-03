8c5059c41827a16785f57caa19346787dfcdd6de
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-05-03 11:24:51 +0200

    Fix app name

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Microsoft.Azure.Management.Subscription.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :azure,
+    [app: :ex_microsoft_azure_management,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
